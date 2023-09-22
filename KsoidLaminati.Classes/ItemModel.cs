80ac614c44e8c9b4daa0e435eba0dc3cf4b39bd7
Molee <milosmoskic147@gmail.com>
2023-09-22 14:39:34 +0200

    Created model for Item

---
+++ KsoidLaminati.Classes/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KsoidLaminati.Classes
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string ItemType { get; set; }
+        public string ItemBrand { get; set; }
+        public string ItemName { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
