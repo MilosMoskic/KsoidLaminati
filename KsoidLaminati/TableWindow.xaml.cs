2e38c56d28ea68f7ee68733ebdb162306ba85ada
Molee <milosmoskic147@gmail.com>
2023-09-22 20:10:53 +0200

    Updated and wired windows

---
+++ KsoidLaminati/TableWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KsoidLaminati.UI
+{
+    /// <summary>
+    /// Interaction logic for TableWindow.xaml
+    /// </summary>
+    public partial class TableWindow : Window
+    {
+        public TableWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackToUI(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMainWindow = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            objMainWindow.Show();
+        }
+    }
+}
