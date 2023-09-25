f0ef2570f4501f17a07782ef08e0d0d5dd467e0c
Molee <milosmoskic147@gmail.com>
2023-09-25 11:40:22 +0200

    Installed Sqlite and Dapper and created query methods with database

---
+++ KsoidLaminati.Classes/SqlDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+
+namespace KsoidLaminati.Classes
+{
+    public class SqlDataAccess
+    {
+        public static List<ItemModel> LoadItems()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<ItemModel>("select * from Item", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        public static void SaveItem(ItemModel item)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Item (ItemType, ItemBrand, ItemName, Quantity) " +
+                    "values (@ItemType, @ItemBrand, @ItemName, @Quantity)", item);
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
