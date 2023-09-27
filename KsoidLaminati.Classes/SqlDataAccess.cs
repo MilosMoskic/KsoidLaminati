ba5fa1492ab7d078f4ee18589af933d67a65a058
Molee <milosmoskic147@gmail.com>
2023-09-27 22:53:23 +0200

    Imeplemented Update method for updating items in table, also ui is implemented

--- KsoidLaminati.Classes/SqlDataAccess.cs
+++ KsoidLaminati.Classes/SqlDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
@@ -15,12 +16,35 @@ namespace KsoidLaminati.Classes
                 return output.ToList();
             }
         }
+        //public static ItemModel FindItem(int id)
+        //{
+        //    using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+        //    {
+        //        string selectQuery = $@"select * from Item where id = {id}";
+        //        var entity = cnn.Query<ItemModel>(selectQuery, id);
+        //        return entity as ItemModel;
+        //    }
+        //}
         public static void SaveItem(ItemModel item)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
             {
                 cnn.Execute("insert into Item (ItemType, ItemBrand, ItemName, Quantity) " +
                     "values (@ItemType, @ItemBrand, @ItemName, @Quantity)", item);
+            }
+        }
+        public static void UpdateItem(ItemModel itemModel)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                string selectQuery = $@"select * from Item where id = {itemModel.Id}";
+                var entity = cnn.Query<ItemModel>(selectQuery, itemModel.Id);
+                if(entity != null)
+                {
+                    string updateQuery = @"update Item Set ItemType = @ItemType, ItemBrand = @ItemBrand, ItemName = @ItemName, Quantity = @Quantity Where id = @Id";
+
+                    cnn.Execute(updateQuery, itemModel);
+                }
             }
         }
         private static string LoadConnectionString(string id = "Default")
--- KsoidLaminati/TableWindow.xaml.cs
+++ KsoidLaminati/TableWindow.xaml.cs
@@ -42,7 +42,14 @@ namespace KsoidLaminati.UI
             item.ItemType = itemTypeText.Text;
             item.ItemBrand = itemBrandText.Text;
             item.ItemName = itemNameText.Text;
-            item.Quantity = Convert.ToDecimal(quantityText.Text);
+            if (quantityText.Text == "")
+            {
+                item.Quantity = 0;
+            }
+            else
+            {
+                item.Quantity = Convert.ToDecimal(quantityText.Text);
+            }
 
             SqlDataAccess.SaveItem(item);
 
@@ -63,14 +70,60 @@ namespace KsoidLaminati.UI
 
         private void DataGridRow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var row = sender as DataGridRow;
-            var item = row.DataContext as ItemModel;
+            try
+            {
 
-            idText.Text = item.Id.ToString();
-            itemTypeText.Text = item.ItemType.ToString();
-            itemBrandText.Text = item.ItemBrand.ToString();
-            itemNameText.Text = item.ItemName.ToString();
-            quantityText.Text = item.Quantity.ToString();
+                var row = sender as DataGridRow;
+                var item = row.DataContext as ItemModel;
+
+                idText.Text = item.Id.ToString();
+                itemTypeText.Text = item.ItemType.ToString();
+                itemBrandText.Text = item.ItemBrand.ToString();
+                itemNameText.Text = item.ItemName.ToString();
+                quantityText.Text = item.Quantity.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void updateItemButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (idText.Text != "")
+                {
+                    ItemModel item = new ItemModel();
+                    item.Id = Convert.ToInt32(idText.Text);
+                    item.ItemType = itemTypeText.Text;
+                    item.ItemName = itemNameText.Text;
+                    item.ItemBrand = itemBrandText.Text;
+                    if (quantityText.Text == "")
+                    {
+                        item.Quantity = 0;
+                    }
+                    else
+                    {
+                        item.Quantity = Convert.ToDecimal(quantityText.Text);
+                    }
+                    SqlDataAccess.UpdateItem(item);
+
+                    itemTypeText.Text = "";
+                    itemBrandText.Text = "";
+                    itemNameText.Text = "";
+                    quantityText.Text = "";
+
+                    DisplayTable();
+                }
+                else
+                {
+                    MessageBox.Show("Id je null.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
